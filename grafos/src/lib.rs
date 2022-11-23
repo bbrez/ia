cb71bfc6e88904a4615b3d1690b21e415643565e
Bruno <bbrezolin@hotmail.com>
2022-11-23 14:44:50 -0300

    mudança na estrutura de grafo

--- grafos/src/lib.rs
+++ grafos/src/lib.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
 #[allow(dead_code)]
+#[derive(Clone, Debug)]
 struct Aresta {
     origem: u32,
     peso: u32,
     destino: u32,
 }
 
+#[allow(dead_code)]
 impl Aresta {
     pub fn new(origem: u32, peso: u32, destino: u32) -> Aresta {
         Aresta {
@@ -15,42 +17,90 @@ impl Aresta {
             destino,
         }
     }
-}
 
-#[allow(dead_code)]
-struct Vertice {
-    id: u32,
-    arestas: HashMap<u32, Aresta>,
-}
-
-impl Vertice {
-    pub fn new(id: u32) -> Vertice {
-        Vertice {
-            id,
-            arestas: HashMap::new(),
-        }
+    pub fn get_peso(self) -> u32 {
+        self.peso
     }
 }
 
 #[allow(dead_code)]
-struct Grafo {
-    vertices: HashMap<u32, Vertice>,
-    orientado: bool,
+#[derive(Clone, Debug)]
+struct Vertice {
+    id: u32,
+    tag: String,
+    arestas: HashMap<u32, Aresta>, //HashMap sendo utilizado como vetor esparso, Arestas são inseridas no indice do id de seu destino
 }
 
+#[allow(dead_code)]
+impl Vertice {
+    pub fn new(id: u32, tag: String) -> Vertice {
+        Vertice {
+            id,
+            tag,
+            arestas: HashMap::new(),
+        }
+    }
+
+    pub fn set_aresta(mut self, peso: u32, destino: u32) {
+        self.arestas
+            .entry(destino)
+            .and_modify(|ar| ar.peso = peso)
+            .or_insert(Aresta::new(self.id, peso, destino));
+    }
+
+    pub fn get_aresta(self, destino: u32) -> Option<Aresta> {
+        self.arestas.get(&destino).cloned()
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+struct Grafo {
+    vertices: Vec<Vertice>, //Vetor de vertices, sempre inseridos linearmente, o Id de um Vertice é sua posição no vetor
+    orientado: bool, //Indica se o Grafo é orientado, se for, toda inserção de aresta chama a inserção de uma aresta em sentido oposto
+}
+
+#[allow(dead_code)]
 impl Grafo {
     pub fn new(orientado: bool) -> Grafo {
         Grafo {
-            vertices: HashMap::new(),
+            vertices: Vec::new(),
             orientado,
         }
     }
 
-    pub fn set_aresta(mut self, origem: u32, peso: u32, destino: u32) {
-        let vert = self.vertices.entry(origem).or_insert(Vertice::new(origem));
-        vert.arestas
-            .entry(destino)
-            .and_modify(|ar| ar.peso = peso)
-            .or_insert(Aresta::new(origem, peso, destino));
+    pub fn add_vertice(mut self, tag: String) {
+        if self
+            .vertices
+            .iter()
+            .find_map(|vert| if vert.tag == tag { Some(vert) } else { None })
+            .is_none()
+        {
+            self.vertices
+                .push(Vertice::new(self.vertices.len().try_into().unwrap(), tag));
+        };
     }
+
+    pub fn get_vertice(&self, tag: String) -> Option<&Vertice> {
+        self.vertices
+            .iter()
+            .find_map(|vert| if vert.tag == tag { Some(vert) } else { None })
+    }
+
+    pub fn add_aresta(mut self, origem: u32, peso: u32, destino: u32) {}
+
+    // pub fn set_aresta(mut self, origem: u32, peso: u32, destino: u32) {
+    //     let vert = self.vertices.entry(origem).or_insert(Vertice::new(origem));
+    //     vert.arestas
+    //         .entry(destino)
+    //         .and_modify(|ar| ar.peso = peso)
+    //         .or_insert(Aresta::new(origem, peso, destino));
+    // }
+
+    // pub fn get_aresta(self, origem: u32, destino: u32) -> Option<Aresta> {
+    //     match self.vertices.get(&origem) {
+    //         Some(vert) => vert.arestas.get(&destino).cloned(),
+    //         None => None,
+    //     }
+    // }
 }
